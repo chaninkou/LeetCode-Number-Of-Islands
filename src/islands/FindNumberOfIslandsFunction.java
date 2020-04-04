e4caad2167fb68fffbd2dbffa29a29f5a0c97f2c
chaninkou <michaelkoulove@yahoo.com>
2020-04-04 13:54:24 -0700

    Redid 2D array problem using dfs to flip 1 to 0 as visited and traversal

--- src/islands/FindNumberOfIslandsFunction.java
+++ src/islands/FindNumberOfIslandsFunction.java
@@ -12,13 +12,13 @@ public class FindNumberOfIslandsFunction {
 
 		// loop through the whole grid
 		// n x m since the col and row doesn't need to be the same
-		for (int i = 0; i < grid.length; i++) {
-			for (int j = 0; j < grid[i].length; j++) {
+		for (int row = 0; row < grid.length; row++) {
+			for (int col = 0; col < grid[row].length; col++) {
 				// If we see 1, thats mean it is an island
-				if (grid[i][j] == '1') {
-					// Make sure to add, not equal
+				if (grid[row][col] == '1') {
+					// We could've just numOfIslands++ and make dfs as a void method
 					// This will always add 1 no matter what
-					numOfIslands += dfs(grid, i, j);
+					numOfIslands += dfs(grid, row, col);
 				}
 			}
 		}
@@ -28,36 +28,32 @@ public class FindNumberOfIslandsFunction {
 
 	// Main point of dfs is to switch all 1 to 0 that is connected
 	// This dfs will just go down from current element and swap any 1 to zero, always return 1 when the recursive is done
-	// Reason why there is Down, Up, right, left because an islands could be all 1's next to each other
-	public int dfs(char[][] grid, int i, int j) {
-		// i < 0, means it went above the grid
-		// i >= grid.length, means we went below the grid
-		// j < 0, means when left out of the grid
-		// j >= grid[i].length, mean we went right out of the grid
-		// grid[i][j] == '0', if its 0, we don't care
-		if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == '0') {
+	public int dfs(char[][] grid, int row, int col) {
+		// row < 0, means it went above the grid
+		// row >= grid.length, means we went below the grid
+		// col < 0, means when left out of the grid
+		// col >= grid[row].length, mean we went right out of the grid
+		// grid[row][col] == '0', if its 0, we don't care
+		if (row < 0 || row >= grid.length || col < 0 || col >= grid[row].length || grid[row][col] == '0') {
 			// return 0 does not do anything, just a place holder
 			return 0;
 		}
 
 		// If the index we are on is 1, then set it to 0 so we don't revisit it
-		grid[i][j] = '0';
+		grid[row][col] = '0';
 
-		// Make sense to go down, top, right, left since we start from index 0
-		
 		// Down
-		dfs(grid, i + 1, j);
+		dfs(grid, row + 1, col);
 
 		// Up
-		dfs(grid, i - 1, j);
+		dfs(grid, row - 1, col);
 
 		// To the right
-		dfs(grid, i, j + 1);
+		dfs(grid, row, col + 1);
 
 		// To the left
-		dfs(grid, i, j - 1);
+		dfs(grid, row, col - 1);
 
-		
 		// Return 1 when we complete marking the island
 		return 1;
 	}
